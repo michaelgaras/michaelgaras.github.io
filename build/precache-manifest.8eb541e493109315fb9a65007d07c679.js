2ddc998fa97e53249184255058ac8bcaed97a38c
Michael Garas <michael.garas@bell.ca>
2020-12-18 13:11:54 -0500

    Added CNAME

---
+++ build/precache-manifest.8eb541e493109315fb9a65007d07c679.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5dc15a63f7889eb9a170d4087f7ed9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1ec9f33e38318d694283",
+    "url": "/portfolio/static/css/2.188d5335.chunk.css"
+  },
+  {
+    "revision": "92bf108335aa4722be95",
+    "url": "/portfolio/static/css/main.cd7c694c.chunk.css"
+  },
+  {
+    "revision": "1ec9f33e38318d694283",
+    "url": "/portfolio/static/js/2.aebccef0.chunk.js"
+  },
+  {
+    "revision": "92bf108335aa4722be95",
+    "url": "/portfolio/static/js/main.42308ac1.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "718c9ea69f1b85b13250dffc36915c89",
+    "url": "/portfolio/static/media/MG_Resume.718c9ea6.pdf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);

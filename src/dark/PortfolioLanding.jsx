fe8dd384060a93507524c52265424834e1b11058
Michael Garas <michael.garas@bell.ca>
2021-01-18 12:06:17 -0500

    Fixed dependencies errors

--- src/dark/PortfolioLanding.jsx
+++ src/dark/PortfolioLanding.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import ScrollToTop from "react-scroll-up";
 import Particles from "react-particles-js";
 import { FiChevronUp } from "react-icons/fi";
@@ -24,155 +24,159 @@ const SlideList = [
     buttonLink: resume,
   },
 ];
-const PortfolioLanding = () => {
-  let title = "About Me",
-    description = `A 2020 McMaster Electrical Engineering and Management graduate with 3+ years of working experience. Highly involved in Software Full Stack Development and DevOps roles, including Web Development and Application Development.
+
+class PortfolioLanding extends Component {
+  render() {
+    let title = "About Me",
+      description = `A 2020 McMaster Electrical Engineering and Management graduate with 3+ years of working experience. Highly involved in Software Full Stack Development and DevOps roles, including Web Development and Application Development.
 
 "Believe you can and you're halfway there." `;
-  const PostList = BlogContent.slice(0, 3);
-  return (
-    <div className="active-dark">
-      <Helmet pageTitle="Portfolio Landing" />
+    const PostList = BlogContent.slice(0, 3);
+    return (
+      <div className="active-dark">
+        <Helmet pageTitle="Portfolio Landing" />
 
-      <HeaderThree homeLink="/" logo="symbol-dark" color="color-black" />
-      {/* Start Slider Area   */}
-      <div id="home" className="fix">
-        <div className="slider-wrapper">
-          {/* Start Single Slide */}
-          <div
-            className="slider-activation slider-creative-agency with-particles"
-            id="home"
-          >
-            <div className="frame-layout__particles">
-              <Particles
-                params={{
-                  particles: {
-                    number: {
-                      value: 50,
-                    },
-                    size: {
-                      value: 4,
-                    },
-                  },
-                  interactivity: {
-                    events: {
-                      onhover: {
-                        enable: true,
-                        mode: "repulse",
+        <HeaderThree homeLink="/" logo="symbol-dark" color="color-black" />
+        {/* Start Slider Area   */}
+        <div id="home" className="fix">
+          <div className="slider-wrapper">
+            {/* Start Single Slide */}
+            <div
+              className="slider-activation slider-creative-agency with-particles"
+              id="home"
+            >
+              <div className="frame-layout__particles">
+                <Particles
+                  params={{
+                    particles: {
+                      number: {
+                        value: 50,
+                      },
+                      size: {
+                        value: 4,
                       },
                     },
-                  },
-                }}
-              />
-            </div>
-            {SlideList.map((value, index) => (
-              <div
-                className="slide personal-portfolio-slider slider-paralax slider-style-3 d-flex align-items-center justify-content-center bg_image bg_image--25"
-                key={index}
-              >
-                <div className="container">
-                  <div className="row">
-                    <div className="col-lg-12">
-                      <div className={`inner ${value.textPosition}`}>
-                        {value.category ? (
-                          <span className="custom-quote">{value.category}</span>
-                        ) : (
-                          ""
-                        )}
-                        <h1 className="title">
-                          Hi, I’m Michael Garas <br />
-                          <TextLoop>
-                            <Typist>
-                              <span>Application Developer.</span>
-                            </Typist>
+                    interactivity: {
+                      events: {
+                        onhover: {
+                          enable: true,
+                          mode: "repulse",
+                        },
+                      },
+                    },
+                  }}
+                />
+              </div>
+              {SlideList.map((value, index) => (
+                <div
+                  className="slide personal-portfolio-slider slider-paralax slider-style-3 d-flex align-items-center justify-content-center bg_image bg_image--25"
+                  key={index}
+                >
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-lg-12">
+                        <div className={`inner ${value.textPosition}`}>
+                          {value.category ? (
+                            <span className="custom-quote">
+                              {value.category}
+                            </span>
+                          ) : (
+                            ""
+                          )}
+                          <h1 className="title">
+                            Hi, I’m Michael Garas <br />
+                            <TextLoop>
+                              <Typist>
+                                <span>Application Developer.</span>
+                              </Typist>
 
-                            <Typist>
-                              <span>Web Developer.</span>
-                            </Typist>
+                              <Typist>
+                                <span>Web Developer.</span>
+                              </Typist>
 
-                            <Typist>
-                              <span>Full Stack Developer.</span>
-                            </Typist>
+                              <Typist>
+                                <span>Full Stack Developer.</span>
+                              </Typist>
 
-                            <Typist>
-                              <span>Wordpress, Wix.</span>
-                            </Typist>
+                              <Typist>
+                                <span>Wordpress, Wix.</span>
+                              </Typist>
 
-                            <Typist>
-                              <span>Weebly, Shopify.</span>
-                            </Typist>
-                            {/* <span> JS Developer.</span>
+                              <Typist>
+                                <span>Weebly, Shopify.</span>
+                              </Typist>
+                              {/* <span> JS Developer.</span>
                                                         <span> UI/UX Designer.</span>
                                                         <span> Content Writter.</span> */}
-                          </TextLoop>{" "}
-                        </h1>
-                        <h2>based in Toronto.</h2>
-                        {value.description ? (
-                          <p className="description">{value.description}</p>
-                        ) : (
-                          ""
-                        )}
-                        {value.buttonText ? (
-                          <div className="slide-btn">
-                            <a
-                              className="rn-button-style--2 btn-primary-color"
-                              href={`${value.buttonLink}`}
-                              target="_blank"
-                            >
-                              {value.buttonText}
-                            </a>
-                          </div>
-                        ) : (
-                          ""
-                        )}
+                            </TextLoop>{" "}
+                          </h1>
+                          <h2>based in Toronto.</h2>
+                          {value.description ? (
+                            <p className="description">{value.description}</p>
+                          ) : (
+                            ""
+                          )}
+                          {value.buttonText ? (
+                            <div className="slide-btn">
+                              <a
+                                className="rn-button-style--2 btn-primary-color"
+                                href={`${value.buttonLink}`}
+                                target="_blank"
+                              >
+                                {value.buttonText}
+                              </a>
+                            </div>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            {/* End Single Slide */}
+          </div>
+        </div>
+        {/* End Slider Area   */}
+
+        {/* Start About Area */}
+        <div id="about" className="fix">
+          <div className="about-area ptb--120  bg_color--1">
+            <div className="about-wrapper">
+              <div className="container">
+                <div className="row row--35 align-items-center">
+                  <div className="col-lg-5">
+                    <div className="thumbnail">
+                      <img
+                        className="w-100"
+                        src="/assets/images/about/mg-1.jpg"
+                        alt="About Images"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-7">
+                    <div className="about-inner inner">
+                      <div className="section-title">
+                        <h2 className="title">{title}</h2>
+                        <p className="description">{description}</p>
+                      </div>
+                      <div className="row mt--30">
+                        <TabTwo tabStyle="tab-style--1" />
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
-          {/* End Single Slide */}
-        </div>
-      </div>
-      {/* End Slider Area   */}
-
-      {/* Start About Area */}
-      <div id="about" className="fix">
-        <div className="about-area ptb--120  bg_color--1">
-          <div className="about-wrapper">
-            <div className="container">
-              <div className="row row--35 align-items-center">
-                <div className="col-lg-5">
-                  <div className="thumbnail">
-                    <img
-                      className="w-100"
-                      src="/assets/images/about/mg-1.jpg"
-                      alt="About Images"
-                    />
-                  </div>
-                </div>
-                <div className="col-lg-7">
-                  <div className="about-inner inner">
-                    <div className="section-title">
-                      <h2 className="title">{title}</h2>
-                      <p className="description">{description}</p>
-                    </div>
-                    <div className="row mt--30">
-                      <TabTwo tabStyle="tab-style--1" />
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* End About Area */}
+        {/* End About Area */}
 
-      {/* Start Service Area  */}
-      {/* <div id="service" className="fix">
+        {/* Start Service Area  */}
+        {/* <div id="service" className="fix">
         <div className="service-area creative-service-wrapper ptb--120 bg_color--5">
           <div className="container">
             <div className="row">
@@ -197,45 +201,45 @@ const PortfolioLanding = () => {
           </div>
         </div>
       </div> */}
-      {/* End Service Area  */}
+        {/* End Service Area  */}
 
-      {/* Start Portfolio Area */}
-      <div id="portfolio" className="fix">
-        <div className="portfolio-area ptb--120 bg_color--1">
-          <div className="portfolio-sacousel-inner">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="section-title text-center service-style--3 mb--30 mb_sm--0">
-                    <h2 className="title">My Latest Project</h2>
-                    <p>Checkout some of my latest software projects.</p>
+        {/* Start Portfolio Area */}
+        <div id="portfolio" className="fix">
+          <div className="portfolio-area ptb--120 bg_color--1">
+            <div className="portfolio-sacousel-inner">
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="section-title text-center service-style--3 mb--30 mb_sm--0">
+                      <h2 className="title">My Latest Project</h2>
+                      <p>Checkout some of my latest software projects.</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="row">
-                <PortfolioList
-                  styevariation="text-center mt--40"
-                  column="col-lg-4 col-md-6 col-sm-6 col-12"
-                  item="6"
-                />
-              </div>
-              <div className="row">
-                <div className="col-lg-12">
-                  {/* <div className="view-more-btn mt--60 mt_sm--30 text-center">
+                <div className="row">
+                  <PortfolioList
+                    styevariation="text-center mt--40"
+                    column="col-lg-4 col-md-6 col-sm-6 col-12"
+                    item="6"
+                  />
+                </div>
+                <div className="row">
+                  <div className="col-lg-12">
+                    {/* <div className="view-more-btn mt--60 mt_sm--30 text-center">
                     <a className="rn-button-style--2 btn-solid" href="/blog">
                       <span>View More</span>
                     </a>
                   </div> */}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* End Portfolio Area */}
+        {/* End Portfolio Area */}
 
-      {/* Start Blog Area */}
-      {/* <div id="blog" className="fix">
+        {/* Start Blog Area */}
+        {/* <div id="blog" className="fix">
         <div className="rn-blog-area ptb--120 bg_color--5 mb-dec--30">
           <div className="container">
             <div className="row align-items-end">
@@ -281,29 +285,30 @@ const PortfolioLanding = () => {
           </div>
         </div>
       </div> */}
-      {/* End Blog Area */}
+        {/* End Blog Area */}
 
-      {/* Start COntact Area */}
-      <div id="contact" className="fix">
-        <div className="rn-contact-area ptb--120 bg_color--1">
-          <ContactThree
-            contactImages="/assets/images/about/mg-1.jpg"
-            contactTitle="Hire Me."
-          />
+        {/* Start COntact Area */}
+        <div id="contact" className="fix">
+          <div className="rn-contact-area ptb--120 bg_color--1">
+            <ContactThree
+              contactImages="/assets/images/about/mg-1.jpg"
+              contactTitle="Hire Me."
+            />
+          </div>
         </div>
-      </div>
-      {/* End COntact Area */}
+        {/* End COntact Area */}
 
-      <FooterTwo />
-      {/* Start Back To Top */}
-      <div className="backto-top">
-        <ScrollToTop showUnder={160}>
-          <FiChevronUp />
-        </ScrollToTop>
+        <FooterTwo />
+        {/* Start Back To Top */}
+        <div className="backto-top">
+          <ScrollToTop showUnder={160}>
+            <FiChevronUp />
+          </ScrollToTop>
+        </div>
+        {/* End Back To Top */}
       </div>
-      {/* End Back To Top */}
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default PortfolioLanding;
--- src/index.js
+++ src/index.js
@@ -9,52 +9,52 @@ import "./index.scss";
 // import Layout from "./component/common/App";
 
 // Home layout
-import Demo from "./page-demo/Demo";
-import MainDemo from "./home/MainDemo";
-import Startup from "./home/Startup";
-import Paralax from "./home/Paralax";
-import HomePortfolio from "./home/HomePortfolio";
-import DigitalAgency from "./home/DigitalAgency";
-import CreativeAgency from "./home/CreativeAgency";
-import PersonalPortfolio from "./home/PersonalPortfolio";
-import Business from "./home/Business";
-import StudioAgency from "./home/StudioAgency";
-import PortfolioLanding from "./home/PortfolioLanding";
-import CreativeLanding from "./home/CreativeLanding";
-import HomeParticles from "./home/HomeParticles";
-import CreativePortfolio from "./home/CreativePortfolio";
-import DesignerPortfolio from "./home/DesignerPortfolio";
-import InteriorLanding from "./home/Interior";
-import CorporateBusiness from "./home/CorporateBusiness";
+// import Demo from "./page-demo/Demo";
+// import MainDemo from "./home/MainDemo";
+// import Startup from "./home/Startup";
+// import Paralax from "./home/Paralax";
+// import HomePortfolio from "./home/HomePortfolio";
+// import DigitalAgency from "./home/DigitalAgency";
+// import CreativeAgency from "./home/CreativeAgency";
+// import PersonalPortfolio from "./home/PersonalPortfolio";
+// import Business from "./home/Business";
+// import StudioAgency from "./home/StudioAgency";
+// import PortfolioLanding from "./home/PortfolioLanding";
+// import CreativeLanding from "./home/CreativeLanding";
+// import HomeParticles from "./home/HomeParticles";
+// import CreativePortfolio from "./home/CreativePortfolio";
+// import DesignerPortfolio from "./home/DesignerPortfolio";
+// import InteriorLanding from "./home/Interior";
+// import CorporateBusiness from "./home/CorporateBusiness";
 
 // Dark Home Layout
-import DarkMainDemo from "./dark/MainDemo";
+// import DarkMainDemo from "./dark/MainDemo";
 import DarkPortfolioLanding from "./dark/PortfolioLanding";
 
 // Element Layout
-import Service from "./elements/Service";
-import ServiceDetails from "./elements/ServiceDetails";
-import About from "./elements/About";
-import Contact from "./elements/Contact";
-import PortfolioDetails from "./elements/PortfolioDetails";
-import Blog from "./elements/Blog";
-import BlogDetails from "./elements/BlogDetails";
+// import Service from "./elements/Service";
+// import ServiceDetails from "./elements/ServiceDetails";
+// import About from "./elements/About";
+// import Contact from "./elements/Contact";
+// import PortfolioDetails from "./elements/PortfolioDetails";
+// import Blog from "./elements/Blog";
+// import BlogDetails from "./elements/BlogDetails";
 import error404 from "./elements/error404";
 
 // Blocks Layout
 
-import Team from "./blocks/Team";
-import Counters from "./blocks/Counters";
-import Testimonial from "./blocks/Testimonial";
-import Portfolio from "./blocks/Portfolio";
-import VideoPopup from "./blocks/VideoPopup";
-import Gallery from "./blocks/Gallery";
-import Brand from "./blocks/Brand";
-import ProgressBar from "./blocks/ProgressBar";
-import ContactForm from "./blocks/ContactForm";
-import GoogleMap from "./blocks/GoogleMap";
-import Columns from "./blocks/Columns";
-import PricingTable from "./blocks/PricingTable";
+// import Team from "./blocks/Team";
+// import Counters from "./blocks/Counters";
+// import Testimonial from "./blocks/Testimonial";
+// import Portfolio from "./blocks/Portfolio";
+// import VideoPopup from "./blocks/VideoPopup";
+// import Gallery from "./blocks/Gallery";
+// import Brand from "./blocks/Brand";
+// import ProgressBar from "./blocks/ProgressBar";
+// import ContactForm from "./blocks/ContactForm";
+// import GoogleMap from "./blocks/GoogleMap";
+// import Columns from "./blocks/Columns";
+// import PricingTable from "./blocks/PricingTable";
 
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
@@ -70,7 +70,7 @@ class Root extends Component {
             path={`${process.env.PUBLIC_URL}/`}
             component={DarkPortfolioLanding}
           />
-          <Route
+          {/* <Route
             exact
             path={`${process.env.PUBLIC_URL}/main-demo`}
             component={MainDemo}
@@ -163,7 +163,7 @@ class Root extends Component {
           />
 
           {/* Element Layot */}
-          <Route
+          {/* <Route
             exact
             path={`${process.env.PUBLIC_URL}/service`}
             component={Service}
@@ -197,10 +197,10 @@ class Root extends Component {
             exact
             path={`${process.env.PUBLIC_URL}/blog-details`}
             component={BlogDetails}
-          />
+          />  */}
 
           {/* Blocks Elements  */}
-          <Route
+          {/* <Route
             exact
             path={`${process.env.PUBLIC_URL}/team`}
             component={Team}
@@ -259,10 +259,10 @@ class Root extends Component {
             exact
             path={`${process.env.PUBLIC_URL}/pricing-table`}
             component={PricingTable}
-          />
+          /> */}
 
-          <Route path={`${process.env.PUBLIC_URL}/404`} component={error404} />
-          <Route component={error404} />
+          {/* <Route path={`${process.env.PUBLIC_URL}/404`} component={error404} /> */}
+          {/* <Route component={error404} /> */}
         </Switch>
       </BrowserRouter>
     );
